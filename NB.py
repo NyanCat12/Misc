bff28db571caaac5ed2110b51beddb4758b50ee9
NyanCat12 <ldj.000000@gmail.com>
2017-03-10 11:07:56 +0800

    Add files via upload

---
+++ NB.py
@@ -0,0 +1,81 @@
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn.decomposition import PCA
+from sklearn.metrics import accuracy_score
+
+TrainFile = open('Train.csv', 'r')
+Sample = []
+Label = []
+i = 1
+length = len(TrainFile.readlines())
+TrainFile.seek(0)
+
+while i < (length+1):
+    test = TrainFile.readline()[:-1]
+    a = test.split(';')
+    b = []
+    for x in a[1:-1]:
+        if x == 'NULL':
+            x = 0.0#Replace all 'NULL's
+        b.append(float(x))
+    Sample.append(b)
+    if int(a[-1]) == 0:
+        Label.append(1)#Oh we lost this player!
+    else:
+        Label.append(0)#Yes they logged in the next day!
+    i += 1
+TrainFile.close()
+SampleNum = len(Sample)
+
+#Load the test dataset
+TestFile = open('Test.csv', 'r')
+Player = []
+Retension = []
+i = 1
+length = len(TestFile.readlines())
+TestFile.seek(0)
+while i < (length+1):
+    test = TestFile.readline()[:-1]
+    a = test.split(';')
+    b = []
+    for x in a[1:-1]:
+        if x == 'NULL':
+            x = 0.0
+        b.append(float(x))
+    Player.append(b)
+    if int(a[-1]) == 0:
+        Retension.append(1)
+    else:
+        Retension.append(0)
+    i += 1
+TestFile.close()
+
+#PCA
+pca = PCA()
+pca.fit(Sample)
+ratio = pca.explained_variance_ratio_
+sum_ratio = 0
+i = 1
+for x in ratio:
+    sum_ratio += x
+    if sum_ratio >= 0.99:
+        break
+    else:
+        i += 1
+pca = PCA(n_components = i)
+pca.fit(Sample)
+Sample = pca.transform(Sample)
+Player = pca.transform(Player)
+
+#Building a model
+X = Sample
+y = Label
+Predict = Player
+
+model = GaussianNB()
+model.fit(X,y)
+
+#Predict
+Z = model.predict(Predict)
+
+print (accuracy_score(Retension, Z))
